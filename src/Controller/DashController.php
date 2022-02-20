cfe226c8663df885da7f9aba9261747d5eb4bd4c
Guemer Houssem <houssem.guemer@gmail.com>
2022-02-20 15:11:10 +0100

    [UPD] add top 10 vehicles and filters

--- src/Controller/DashController.php
+++ src/Controller/DashController.php
@@ -3,9 +3,8 @@
 namespace App\Controller;
 
 use App\Entity\Expense;
-use App\Entity\GasStation;
-use App\Entity\Vehicle;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DashController extends AbstractController
@@ -13,17 +12,28 @@ class DashController extends AbstractController
     /**
      * @Route ("/", name="app_dash_index")
      */
-    public function index()
+    public function index(Request $request)
     {
-        $vehicleRepository = $this->getDoctrine()->getRepository(Vehicle::class);
         $expenseRepository = $this->getDoctrine()->getRepository(Expense::class);
-        $gasStationRepository = $this->getDoctrine()->getRepository(GasStation::class);
+
+        $startDate = '2022-01-01 00:00:00';
+        $endDate = '2022-02-28 23:59:59';
+
+        if($request->query->get('startDate')) {
+            $startDate = $request->query->get('startDate');
+        }
+
+        if($request->query->get('endDate')) {
+            $endDate = $request->query->get('endDate');
+        }
 
         return $this->render('pages/index.html.twig', [
-            'sumHT'=>$expenseRepository->sumHT(),
-            'sumTTC'=>$expenseRepository->sumTTC(),
-            'sumHtByCategory'=>$expenseRepository->sumHtByCategory(),
-            'sumTtcByCategory'=>$expenseRepository->sumTtcByCategory(),
+            'sumHT'=>$expenseRepository->sumHT($startDate,$endDate),
+            'sumTTC'=>$expenseRepository->sumTTC($startDate,$endDate),
+            'sumHtByCategory'=>$expenseRepository->sumHtByCategory($startDate,$endDate),
+            'sumTtcByCategory'=>$expenseRepository->sumTtcByCategory($startDate,$endDate),
+            'sumHtByVehicle'=>$expenseRepository->sumHtByVehicle($startDate,$endDate),
+            'sumTtcByVehicle'=>$expenseRepository->sumTtcByVehicle($startDate,$endDate),
         ]);
     }
 
--- src/Repository/ExpenseRepository.php
+++ src/Repository/ExpenseRepository.php
@@ -4,6 +4,7 @@ namespace App\Repository;
 
 use App\Entity\Expense;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\DBAL\Exception;
 use Doctrine\Persistence\ManagerRegistry;
 
 class ExpenseRepository extends ServiceEntityRepository
@@ -14,16 +15,22 @@ class ExpenseRepository extends ServiceEntityRepository
     }
 
     /**
+     * @param $startDate
+     * @param $endDate
      * @return float
      */
-    public function sumHT(): float
+    public function sumHT($startDate,$endDate): float
     {
         $entityManager = $this->getEntityManager();
 
         $query = $entityManager->createQuery(
             'SELECT SUM(e.valueTe)
-            FROM App\Entity\Expense e'
-        );
+            FROM App\Entity\Expense e
+            WHERE e.issuedOn >= :startDate AND e.issuedOn <= :endDate'
+        )->setParameters([
+            "startDate"=>$startDate,
+            "endDate"=>$endDate,
+        ]);
 
         // returns the sum
         $sum = $query->getResult()[0][1];
@@ -36,16 +43,22 @@ class ExpenseRepository extends ServiceEntityRepository
     }
 
     /**
+     * @param $startDate
+     * @param $endDate
      * @return float
      */
-    public function sumTTC(): float
+    public function sumTTC($startDate,$endDate): float
     {
         $entityManager = $this->getEntityManager();
 
         $query = $entityManager->createQuery(
             'SELECT SUM(e.valueTi)
-            FROM App\Entity\Expense e'
-        );
+            FROM App\Entity\Expense e
+            WHERE e.issuedOn >= :startDate AND e.issuedOn <= :endDate'
+        )->setParameters([
+            "startDate"=>$startDate,
+            "endDate"=>$endDate,
+        ]);
 
         // returns the sum
         $sum = $query->getResult()[0][1];
@@ -58,36 +71,104 @@ class ExpenseRepository extends ServiceEntityRepository
     }
 
     /**
+     * @param $startDate
+     * @param $endDate
      * @return array
      */
-    public function sumHtByCategory(): array
+    public function sumHtByCategory($startDate,$endDate): array
     {
         $entityManager = $this->getEntityManager();
 
         $query = $entityManager->createQuery(
             'SELECT e.category,SUM(e.valueTe)
             FROM App\Entity\Expense e
+            WHERE e.issuedOn >= :startDate AND e.issuedOn <= :endDate
             GROUP BY e.category'
-        );
+        )->setParameters([
+            "startDate"=>$startDate,
+            "endDate"=>$endDate,
+        ]);
 
         // returns the sum by category
         return $query->getResult();
     }
 
     /**
+     * @param $startDate
+     * @param $endDate
      * @return array
      */
-    public function sumTtcByCategory(): array
+    public function sumTtcByCategory($startDate,$endDate): array
     {
         $entityManager = $this->getEntityManager();
 
         $query = $entityManager->createQuery(
             'SELECT e.category,SUM(e.valueTi)
             FROM App\Entity\Expense e
+            WHERE e.issuedOn >= :startDate AND e.issuedOn <= :endDate
             GROUP BY e.category'
-        );
+        )->setParameters([
+            "startDate"=>$startDate,
+            "endDate"=>$endDate,
+        ]);
 
         // returns the sum by category
         return $query->getResult();
+    }
+
+    /**
+     * @param $startDate
+     * @param $endDate
+     * @return array
+     * @throws Exception
+     */
+    public function sumHtByVehicle($startDate,$endDate): array
+    {
+        $entityManager = $this->getEntityManager();
+        $conn = $entityManager->getConnection();
+
+        $sql = '
+            SELECT e.vehicle_id,v.plate_number,v.brand,v.model,SUM(e.value_te) AS sum FROM expense e
+            JOIN vehicle v on v.vehicle_id = e.vehicle_id
+            WHERE e.issued_on >= :startDate AND e.issued_on <= :endDate
+            GROUP BY e.vehicle_id
+            ORDER BY SUM(e.value_te) DESC
+            ';
+        $stmt = $conn->prepare($sql);
+
+
+        // returns the sum by vehicle
+        return $stmt->execute([
+            'startDate'=>$startDate,
+            'endDate'=>$endDate
+        ])->fetchAllAssociative();
+    }
+
+    /**
+     * @param $startDate
+     * @param $endDate
+     * @return array
+     * @throws Exception
+     */
+    public function sumTtcByVehicle($startDate,$endDate): array
+    {
+        $entityManager = $this->getEntityManager();
+        $conn = $entityManager->getConnection();
+
+        $sql = '
+            SELECT e.vehicle_id,v.plate_number,v.brand,v.model,SUM(e.value_ti) AS sum FROM expense e
+            JOIN vehicle v on v.vehicle_id = e.vehicle_id
+            WHERE e.issued_on >= :startDate AND e.issued_on <= :endDate
+            GROUP BY e.vehicle_id
+            ORDER BY SUM(e.value_ti) DESC
+            ';
+        $stmt = $conn->prepare($sql);
+
+
+        // returns the sum by vehicle
+        return $stmt->execute([
+            'startDate'=>$startDate,
+            'endDate'=>$endDate
+        ])->fetchAllAssociative();
     }
 }
